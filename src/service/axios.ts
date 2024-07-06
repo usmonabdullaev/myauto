88e9dab9af188a0698bb2ad674810f3f7851a932
Usmon <abdullaevusmon2006@gmail.com>
2024-07-06 13:48:24 +0500

    BASE

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -5,7 +5,7 @@ const LOCAL_URL = "http://localhost:4444/";
 const BASE_URL = "https://myauto-backend.onrender.com/";
 
 const axiosInstance = axios.create({
-  baseURL: LOCAL_URL,
+  baseURL: BASE_URL,
 });
 
 export { axiosInstance };
