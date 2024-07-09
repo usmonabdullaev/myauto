1312ed8e5c6e25dccccf16993af1809ff134a6d8
Usmon <abdullaevusmon2006@gmail.com>
2024-07-09 11:01:35 +0500

    http

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 
-const LOCAL_URL = "http://192.168.1.2:4444/api";
-const BASE_URL = "https://myauto-backend.onrender.com/api";
+const BASE_URL = "http://192.168.1.2:4444/api";
 
 const axiosInstance = axios.create({
-  baseURL: LOCAL_URL,
+  baseURL: BASE_URL,
   headers: {
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImlhdCI6MTcyMDQ5ODUxNywiZXhwIjoxNzIzMDkwNTE3fQ.XzGRc6QW4TdMkatriMLHfpnBXoeb_7-uR9dG5wg46_0",
