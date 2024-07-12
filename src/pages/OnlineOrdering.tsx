049b72c243f9b93fbe09f543128e387d0c47f3b5
Usmon <abdullaevusmon2006@gmail.com>
2024-07-12 11:28:13 +0500

    fix online-ordering

--- src/pages/OnlineOrdering.tsx
+++ src/pages/OnlineOrdering.tsx
@@ -1,14 +1,29 @@
-import { Badge, Button, Input, message } from "antd";
+import { Badge, Button, Input, message, Progress } from "antd";
 import React, { ChangeEvent, useState } from "react";
 import { CameraOutlined, DeleteOutlined } from "@ant-design/icons";
+
 import { filesToBase64 } from "../service/functions.ts";
 
 const OnlineOrdering = () => {
   const [desc, setDesc] = useState("");
   const [imageUrl, setImageUrl] = useState<string[]>([]);
 
-  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
+  const handleChange = async (
+    e?: ChangeEvent<HTMLInputElement>,
+    drop: boolean = false,
+    dropFiles?: File[]
+  ) => {
+    if (drop) {
+      const files = dropFiles;
+      if (files?.length) {
+        if (imageUrl.length + files.length > 5) {
+          return message.warning("Максимум лимит изображение 5");
+        }
+        const base64s = await filesToBase64(files);
+        setImageUrl([...imageUrl, ...base64s]);
+      }
+    }
+    const files = e?.target.files;
     if (files) {
       if (imageUrl.length + Array.from(files).length > 5) {
         return message.warning("Максимум лимит изображение 5");
@@ -51,7 +66,7 @@ const OnlineOrdering = () => {
       </div>
 
       <div className="my-10">
-        <div className="grid grid-cols-12">
+        <div className="grid grid-cols-12 gap-8">
           <div className="col-span-9 bg-white rounded-xl p-4">
             <p className="font-semibold text-lg mb-8">
               Заполните форму, чтобы оформить ваш заказ на запчасти для
@@ -87,19 +102,25 @@ const OnlineOrdering = () => {
                 placeholder="Пожалуйста, укажите необходимые запчасти, их количество и краткое описание"
                 autoSize={{ minRows: 5, maxRows: 10 }}
               />
-              <div className="col-span-12 grid grid-cols-12 gap-4">
+              <Progress
+                percent={imageUrl.length * 20}
+                type="line"
+                className="col-span-12"
+                size={["100%", 12]}
+              />
+              <div className="col-span-12 grid grid-cols-5 gap-4">
                 {imageUrl.map((i, index) => (
-                  <div key={index} className="col-span-2 size-[140px]">
+                  <div key={index} className="col-span-1 size-[140px]">
                     <Badge
                       size="default"
                       className="size-full"
                       count={
                         <span
-                          onClick={() =>
+                          onClick={() => {
                             setImageUrl(
                               imageUrl.filter((_, index2) => index !== index2)
-                            )
-                          }
+                            );
+                          }}
                           className="bg-red-500 rounded-[50%] text-white size-[24px] flex items-center justify-center cursor-pointer"
                         >
                           <DeleteOutlined className="text-[12px]" />
@@ -110,43 +131,95 @@ const OnlineOrdering = () => {
                         src={i}
                         alt="Order"
                         className="size-full object-cover rounded-xl select-none"
-                        onError={(event) => {
-                          // Error handling
+                        onError={() => {
+                          message.warning("Invalid image type");
+                          setImageUrl(
+                            imageUrl.filter((_, index2) => index !== index2)
+                          );
                         }}
                       />
                     </Badge>
                   </div>
                 ))}
-                <label
-                  htmlFor="files"
-                  className="border-dashed size-[140px] col-span-2 cursor-pointer border-[1px] border-gray-500 transition hover:border-sky-500 rounded-xl flex items-center justify-center bg-[#fafafa]"
-                >
-                  <input
-                    type="file"
-                    accept=".png, .jpg, .jpeg, .heic"
-                    onChange={handleChange}
-                    id="files"
-                    multiple
-                    hidden
-                  />
+                {imageUrl.length < 5 && (
                   <label
+                    onDragOver={(e) => e.preventDefault()}
+                    onDrop={(e) => {
+                      e.preventDefault();
+                      const files = Array.from(e.dataTransfer.files);
+                      handleChange(undefined, true, files);
+                    }}
                     htmlFor="files"
-                    style={{ border: 0, background: "none" }}
-                    className="flex flex-col items-center w-full cursor-pointer select-none"
+                    className="border-dashed size-[140px] col-span-1 cursor-pointer border-[1px] border-gray-500 transition hover:border-sky-500 rounded-xl flex items-center justify-center bg-[#fafafa]"
                   >
-                    <CameraOutlined className="text-[32px]" />
-                    <div className="font-[700] mt-2 text-sm text-center">
-                      Загрузить фото запчасти
-                    </div>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      onChange={handleChange}
+                      id="files"
+                      multiple
+                      hidden
+                    />
+                    <label
+                      htmlFor="files"
+                      style={{ border: 0, background: "none" }}
+                      className="flex flex-col items-center w-full cursor-pointer select-none"
+                    >
+                      <CameraOutlined className="text-[32px]" />
+                      <div className="font-[700] mt-2 text-sm text-center">
+                        Загрузить фото запчасти
+                      </div>
+                    </label>
                   </label>
-                </label>
+                )}
               </div>
             </div>
             <Button size="large" type="primary">
               Отправить
             </Button>
           </div>
-          <div className="col-span-3"></div>
+          <div className="col-span-3">
+            <div className="grid grid-cols-1 gap-8 h-full">
+              <div
+                className="col-span-1 rounded-xl bg-[#5946d7] p-6 flex flex-col justify-between"
+                style={{
+                  backgroundImage: "url(/order-banner-1.png)",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              >
+                <p className="text-white text-2xl font-semibold">
+                  Автомасла №1 от официальных дилеров
+                </p>
+                <Button
+                  size="large"
+                  className="text-[#0379d7] font-semibold"
+                  block
+                >
+                  Заказать
+                </Button>
+              </div>
+              <div
+                className="col-span-1 rounded-xl bg-[#242729] p-6 flex flex-col justify-between"
+                style={{
+                  backgroundImage: "url(/order-banner-2.png)",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              >
+                <p className="text-white text-2xl font-semibold">
+                  Купить зимние шины
+                </p>
+                <Button
+                  size="large"
+                  className="text-[#0379d7] font-semibold"
+                  block
+                >
+                  Заказать
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
